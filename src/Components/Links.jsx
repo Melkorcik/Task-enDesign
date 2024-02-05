ef45a7343fe760a757d1b4634d78bb7444f85ff4
IgorRev <rewcsharp@gmail.com>
2024-02-05 11:44:09 +0300

    add try catch and replaced obj.id on obj.userId

--- src/Components/Links.jsx
+++ src/Components/Links.jsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
 import '../pages/Invisible.css';
 
-const Links = ({id, name, body}) => {
+const Links = ({id, name, title, body}) => {
     return(
         <div className="card">
          <header className="header-card">
             <Link to={`/users/${id}`} className="name-Link"><li><strong>{name}</strong></li></Link>
          </header>
          <span className="box-text">
+            <h3>{title}</h3>
             {body}
          </span>
         </div>
--- src/pages/Invisible.jsx
+++ src/pages/Invisible.jsx
@@ -3,27 +3,30 @@ import Links from "../Components/Links";
 
 const Invisible = () => {
     const[posts, setPosts] = useState([]);
-
+    
     useEffect(()=>{
-        async function FetchUsers(){
-            //users
-            const requestUsers = await fetch('https://jsonplaceholder.typicode.com/users');
-            const responseUsers = await requestUsers.json().catch(err => console.error(err));
-            //posts
-            const requestPosts = await fetch('https://jsonplaceholder.typicode.com/posts');
-            const responsePosts = await requestPosts.json().catch(err => console.error(err));
-            
-            //use filter/find
-            setPosts(
-              responsePosts.filter(obj=> {
-                return responseUsers.find(obj2 => {
-                    if(obj.id === obj2.id) return Object.assign(obj, obj2);
-                });
-              })
-            );
-      
-          }
-          FetchUsers();
+        async function FetchUsers(){            
+            try {
+                const requestUsers = await fetch('https://jsonplaceholder.typicode.com/users');
+                const responseUsers = await requestUsers.json();
+
+                const requestPosts = await fetch('https://jsonplaceholder.typicode.com/posts');
+                const responsePosts = await requestPosts.json();
+
+                //filter/find 
+                
+                setPosts(                    
+                    responsePosts.filter(obj=> {
+                        return responseUsers.find(obj2 => {
+                            if(obj.userId === obj2.id) return Object.assign(obj, obj2);
+                        });
+                    })                    
+                )
+            } catch (error) {
+                console.log(error);
+            }      
+        }
+        FetchUsers();
     },[])
          
     return(
@@ -31,7 +34,7 @@ const Invisible = () => {
             {
                 posts.map(el => {
                     return(                            
-                        <Links key={el.name} id={el.id} name={el.name} body={el.body}/>                            
+                        <Links key={el.name} id={el.id} name={el.name} title={el.title} body={el.body}/>                            
                     )
                 })
             }
